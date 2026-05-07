0d67447e9a6a1544f0ca0906dbdb0bc3de796af6
ZJarjur <zjarjur@gmail.com>
2026-05-07 14:14:36 +0800

    Calculator Stage 5

--- scripts/counter.js
+++ scripts/counter.js
@@ -2,6 +2,13 @@
 
 let counter = document.getElementById("counter");
 let number = 0;
+let memory = 0;
+let displacement = 0;
+let numType = "integer";
+let operatorType = "plus";
+
+
+//Make Button Variables
 let buttonPlusOne = document.getElementById("counter-button");
 let buttonMinusOne = document.getElementById("minus-one-button");
 let clearButton = document.getElementById("clear-button");
@@ -14,77 +21,124 @@ let sixButton = document.getElementById("six-button");
 let sevenButton = document.getElementById("seven-button");
 let eightButton = document.getElementById("eight-button");
 let nineButton = document.getElementById("nine-button");
+let plusButton = document.getElementById("plus-button");
 let zeroButton = document.getElementById("zero-button");
+let equalButton = document.getElementById("equal-button");
+let decimalButton = document.getElementById("decimal-button");
 
 //Define Functions
-function show(something){
-    counter.textContent = something;
+function makeDecimal(aNumber, decPlaces) {
+    return (aNumber * (0.1 ** decPlaces));
 }
 
-function addOne(){
+function decimalPoint() {
+    numType = "decimal";
+}
+
+function decCount() {
+    if (numType === "decimal") {
+        displacement = displacement + 1;
+    }
+}
+
+function show(something) {
+    counter.textContent = something.toFixed(displacement);
+}
+
+function addOne() {
     number = number + 1;
-    show(number);
+    show(makeDecimal(number,displacement));
 }
 
-function minusOne(){
+function minusOne() {
     number = number - 1;
-    show(number);
+    show(makeDecimal(number,displacement));
 }
 
-function clear(){
+function clear() {
     number = 0;
-    show(number);
+    numType = "integer";
+    displacement = 0;
+    show(makeDecimal(number,displacement));
 }
 
-function one1(){
+function one1() {
     number = number * 10 + 1;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function two2(){
+function two2() {
     number = number * 10 + 2;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function three3(){
+function three3() {
     number = number * 10 + 3;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function four4(){
+function four4() {
     number = number * 10 + 4;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function five5(){
+function five5() {
     number = number * 10 + 5;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function six6(){
+function six6() {
     number = number * 10 + 6;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function seven7(){
+function seven7() {
     number = number * 10 + 7;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function eight8(){
+function eight8() {
     number = number * 10 + 8;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function nine9(){
+function nine9() {
     number = number * 10 + 9;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
 
-function zero0(){
+function zero0() {
     number = number * 10 + 0;
-    show(number);
+    decCount();
+    show(makeDecimal(number,displacement));
 }
+
+function plus() {
+    memory = makeDecimal(number,displacement);
+    number = 0;
+    numType = "integer";
+    operatorType = "plus";
+    displacement = 0;
+}
+
+function equalsTo(){
+    let decNumber = 0.0;
+    if (operatorType === "plus"){
+        decNumber = makeDecimal(number,displacement);
+        number = decNumber + memory;
+        show(number);
+    }
+}
+
 //Define Button Event Listeners
 
 buttonPlusOne.addEventListener("click", addOne);
@@ -100,3 +154,7 @@ sevenButton.addEventListener("click", seven7);
 eightButton.addEventListener("click", eight8);
 nineButton.addEventListener("click", nine9);
 zeroButton.addEventListener("click", zero0);
+plusButton.addEventListener("click", plus);
+equalButton.addEventListener("click", equalsTo);
+decimalButton.addEventListener("click", decimalPoint);
+
